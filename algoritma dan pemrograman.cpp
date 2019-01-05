d481dae0ee0fe9bea60659d82431191a65e514fb
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 15:00:32 +0700

    Add files via upload

---
+++ algoritma dan pemrograman.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n=15;
+    cout<<"=================================\n"<<endl;
+    cout<<"====ALGORITMA DAN PEMROGRAMAN====\N"<<endl;
+    cout<<"=================================\n"<<endl;
+
+    for(i=1; i<=n; i++)
+        cout<<i<<" ALGORITMA DAN PEMROGRAMAN"<<endl;
+    return 0;
+}
